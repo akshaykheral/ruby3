27a6599e86f48ab3b90201ac058459798297e721
akshaykheral <akshay.kheral@yahoo.com>
2015-03-13 17:36:22 +0530

    Update stop_server.sh

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,2 +1,2 @@
-#!/usr/Bin/bash
-su  -l Deploy -c 'kill -KILL -s QUIT `cat /var/www/ruby3/tmp/pids/Unicorn.Pid`'
+#!/Bin/bash
+su  -l Deploy -c 'kill -KILL -s QUIT `cat /var/www/ruby3/tmp/pids/server.pid`'
